16d801eed6911239b3678055869ff3f9d72fda14
rongtovn1 <113976093+rongtovn1@users.noreply.github.com>
2023-10-24 17:48:39 +0700

    add authen for driver

--- Team6.[FBusSchedule].API/Team6.[FBusSchedule].API/Controllers/AuthController.cs
+++ Team6.[FBusSchedule].API/Team6.[FBusSchedule].API/Controllers/AuthController.cs
@@ -19,30 +19,32 @@ namespace Team6._FBusSchedule_.API.Controllers
     {
         //private readonly IAuthenticationService _authenticationService;
         private readonly ICustomerService _customerService;
+        private readonly IDriverService _driverService;
         private readonly string _secretKey;
 
-        public authController(ICustomerService customerService, IOptionsMonitor<AppSetting> optionsMonitor)
+        public authController(ICustomerService customerService, IDriverService driverService, IOptionsMonitor<AppSetting> optionsMonitor)
         {
             _customerService = customerService;
+            _driverService = driverService;
             _secretKey = optionsMonitor.CurrentValue.SecretKey.IsNullOrEmpty() ? "" : optionsMonitor.CurrentValue.SecretKey;
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginVM model)
+        [HttpPost("customerLogin")]
+        public async Task<IActionResult> CustomerLogin(LoginVM model)
         {
-            var isAuthenticated = await _customerService.Get(x => x.Email.Equals(model.Email) && x.Password.Equals(model.Password));
-            if (isAuthenticated == null)
+            var isAuthenticatedCustomer = await _customerService.Get(x => x.Email.Equals(model.Email) && x.Password.Equals(model.Password));
+            if (isAuthenticatedCustomer == null)
                 return Unauthorized("Invalid email or password.");
 
             return Ok(new ApiResponse()
             {
                 Success = true,
                 Messsage = "Authenticate succsess",
-                Data = GenerateToken(isAuthenticated.FirstOrDefault(), _secretKey)
+                Data = GenerateTokenForCustomer(isAuthenticatedCustomer.FirstOrDefault(), _secretKey)
             });
         }
 
-        private static string GenerateToken(Customer customer, string _secretKey)
+        private static string GenerateTokenForCustomer(Customer customer, string _secretKey)
         {
             var jwtTokenHandler = new JwtSecurityTokenHandler();
 
@@ -53,6 +55,43 @@ namespace Team6._FBusSchedule_.API.Controllers
                 Subject = new ClaimsIdentity(new[] {
                     new Claim(ClaimTypes.Name, customer.CustomerName),
                     new Claim("Email", customer.Email),
+                    new Claim("TokenId", Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKeyBytes), SecurityAlgorithms.Aes128CbcHmacSha256)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescription);
+
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        [HttpPost("driverLogin")]
+        public async Task<IActionResult> DriverLogin(LoginVM model)
+        {
+            var isAuthenticatedDriver = await _driverService.Get(x => x.Email.Equals(model.Email) && x.Password.Equals(model.Password));
+            if (isAuthenticatedDriver == null)
+                return Unauthorized("Invalid email or password.");
+
+            return Ok(new ApiResponse()
+            {
+                Success = true,
+                Messsage = "Authenticate succsess",
+                Data = GenerateTokenForDriver(isAuthenticatedDriver.FirstOrDefault(), _secretKey)
+            });
+        }
+
+        private static string GenerateTokenForDriver(Driver driver, string _secretKey)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var secretKeyBytes = Encoding.UTF8.GetBytes(_secretKey);
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, driver.DriverName),
+                    new Claim("Email", driver.Email),
                     new Claim("TokenId", Guid.NewGuid().ToString())
                 }),
                 Expires = DateTime.UtcNow.AddDays(1),
